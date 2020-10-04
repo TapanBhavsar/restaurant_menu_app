2008f780f7770fdffe231f4ce0d620e00f06223f
TapanBhavsar <tapanbhavsar1601@gmail.com>
2020-10-05 00:40:57 +0530

    add sign up page

--- resraurant_menu_app.py
+++ resraurant_menu_app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def log_in():
     return render_template("login.html")
 
+@app.route('/sign_up')
+def sign_up():
+    return render_template("sign_up.html")
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
