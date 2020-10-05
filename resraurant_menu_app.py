0d63522dd718512824228a0cf86bae96418abd3c
TapanBhavsar <tapanbhavsar1601@gmail.com>
2020-10-06 01:24:43 +0530

    access form from the flask server

--- resraurant_menu_app.py
+++ resraurant_menu_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -10,6 +10,21 @@ def log_in():
 @app.route('/sign_up')
 def sign_up():
     return render_template("sign_up.html")
+
+@app.route('/login_validation', methods=['POST'])
+def login_validation():
+        user_name = request.form['username']
+        password = request.form['password']
+        app.logger.debug(f"user name is {user_name}\n password is {password}")
+        return redirect('/')
+
+@app.route('/add_signup', methods = ['POST'])
+def add_signup():
+    user_name = request.form['username']
+    password = request.form['password']
+    password_veified = request.form['password_veified']
+    app.logger.debug(f"user name is {user_name}.\t password is {password}.\t varified password {password_veified}")
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
